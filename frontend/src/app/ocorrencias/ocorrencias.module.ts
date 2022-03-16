2486fb4a3f5687b67ee211b5774b522f3a41f26a
kawps90 <kaue.ps90@protonmail.com>
2022-03-15 23:53:35 -0300

    first commit

---
+++ frontend/src/app/ocorrencias/ocorrencias.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OcorrenciasComponent } from './ocorrencias/ocorrencias.component';
+
+
+
+@NgModule({
+  declarations: [
+    OcorrenciasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OcorrenciasModule { }
